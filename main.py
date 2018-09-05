43965b0d5d8a4ae1ae69872387926b22204ffeda
goodseeyou <goodseeyou@gmail.com>
2018-09-05 23:55:44 +0800

    hears game shell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Coach import Coach
-from othello.OthelloGame import OthelloGame as Game
+from hearts.HeartsGame import HeartsGame as Game
 from othello.pytorch.NNet import NNetWrapper as nn
 from utils import *
 
